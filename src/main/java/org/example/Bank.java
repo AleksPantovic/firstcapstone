894ef6d092835b79fc710b450bd71540f652b01f
AleksPantovic <aleksandar.pantovic@asioso.com>
2025-01-14 13:00:13 +0100

    fixed some logic on both parts

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
+    private String account;
+    private double balance;
+    private List<String> transactions;
+    public Bank(String account,double balance)
+    {
+        this.account=account;
+        this.balance=balance;
+        this.transactions=new ArrayList<>();
+        this.transactions.add("Created account with balance "+balance);
+    }
+    public double CheckBalance(){
+        return balance;
+    }
+    public void deposit(double money){
+        if(money>0) {
+            balance += money;
+            transactions.add("Added " + money + " to the account, current balance is " + balance);
+        }
+        else
+            System.out.println("Cant deposit negative money");
+    }
+    public boolean withdraw(double money)
+    {
+        if(money>0 && money <= balance) {
+            balance -= money;
+            transactions.add("Withdrew " + money + " from the account, current balance is " + balance);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public List<String> getTransactions() {
+        return transactions;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,64 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner=new Scanner(System.in);
+        Bank account= new Bank("Aleksandar Pantovic",500);
+        int choice;
+        do {
+            System.out.println("\nBanking Application Menu");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Show Previous Transactions");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                choice = 0;
+            }
+            switch (choice) {
+                case 1:
+                    System.out.println("Current Balance: " + account.CheckBalance());
+                    break;
+                case 2:
+                    System.out.print("Enter deposit amount: ");
+                    double depositAmount = Double.parseDouble(scanner.nextLine());
+                    account.deposit(depositAmount);
+                    break;
+                case 3:
+                    System.out.print("Enter withdrawal amount: ");
+                    double withdrawalAmount = Double.parseDouble(scanner.nextLine());
+                    if(account.withdraw(withdrawalAmount)) {
+                        System.out.println("Withdrawal successful.");
+                    }
+                    else {
+                        System.out.println("Insufficient funds.");
+                    }
+                    break;
+                case 4:
+                    List<String> transactions = account.getTransactions();
+                    if (transactions.isEmpty()) {
+                        System.out.println("No previous transactions found.");
+                    } else {
+                        System.out.println("Previous Transactions:");
+                        for (String transaction : transactions) {
+                            System.out.println(transaction);
+                        }
+                    }
+                    break;
+                case 5:
+                    System.out.println("Exiting application.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 5);
+        scanner.close();
     }
 }
